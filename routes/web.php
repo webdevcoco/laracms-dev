a96d3e21886c50fd25b70d782b1cb3db67308802
webdevcoco <jonathanentila@gmail.com>
2020-12-07 10:38:17 +0800

    node js install

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::get('/user', [UserController::class, 'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
